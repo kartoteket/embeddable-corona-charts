d9389acaec1499536d63eef85c3a1794f36bf60d
Svale Fossåskaret <svale@3by5.no>
2020-03-19 11:51:45 +0100

    Prettier atuo-formatting

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,35 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 //! URL charts.assets.kartoteket.as/covid19/embeds/charts/taiwan*,norway/combined
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/embeds/chart/:type?/:slug?/:sub?",
+    path: '/embeds/chart/:type?/:slug?/:sub?',
     component: () =>
       import(
-        /* webpackChunkName: "embeds-charts-type-slug-sub" */ "../views/Embeds.vue"
+        /* webpackChunkName: "embeds-charts-type-slug-sub" */ '../views/Embeds.vue'
       )
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
 });
