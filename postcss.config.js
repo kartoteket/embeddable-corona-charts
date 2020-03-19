9ca79a436a6577425b27b8d4315cd5ecb70327fb
Svale Fossåskaret <svale@3by5.no>
2020-03-19 11:54:39 +0100

    Update purgeCSS whitelists

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    'vue-cli-plugin-tailwind/purgecss': {}
+    'vue-cli-plugin-tailwind/purgecss': {
+      whitelist: ['v-select', 'tooltip', 'legend', 'axis'],
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /vs__/,
+        /vs--/
+      ]
+    }
   }
 };
