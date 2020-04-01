8daac7141b6a6c7c3cb1e98ce23966e3cf795fda
Svale Fossåskaret <svale@3by5.no>
2020-04-01 15:18:24 +0200

    Add target attr to source-links

--- src/views/Norway.vue
+++ src/views/Norway.vue
@@ -26,6 +26,7 @@
     <p v-if="!isLoading" class="text-xs ml-4">
       Kilde:
       <a
+        target="_parent"
         class="underline"
         href="https://www.fhi.no/sv/smittsomme-sykdommer/corona/dags--og-ukerapporter/dags--og-ukerapporter-om-koronavirus/"
         >FHI
@@ -33,6 +34,7 @@
       <span v-if="feature === 'scenarios'">
         /
         <a
+          target="_parent"
           class="underline"
           href="https://www.fhi.no/contentassets/c9e459cd7cc24991810a0d28d7803bd0/covid-19-epidemien-risiko-prognose-og-respons-i-norge-etter-uke-12.--24.mars-2020.pdf"
           >FHIs Risikoprognose
